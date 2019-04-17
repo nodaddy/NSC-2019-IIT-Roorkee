e5af1258ca16800a2b17415995adecb4702d165d
Neelesh <nsharma1@me.iitr.ac.in>
2019-04-17 17:46:00 +0530

    conferenceRegistration, sponsorship  page added, some updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,8 +2,8 @@ import IITRLogo from "./IITR_new_logo_color.png"
 import SSILogo from "./ssi-logo.png"
 import Pic1 from "./top1.jpg"
 import Pic2 from "./1300_400.JPG"
-import springerguide from "./Springer_Guidelines_for_Authors (1).pdf"
-import conferenceregistration from "./conferenceregistration.5fd38c92 (1)-converted.pdf"
+import springerguide from "./Springer_Guidelines_for_Authors(1).pdf"
+import conferenceregistration from "./conferenceregistration_5fd38c92(1)_converted.pdf"
 import springerlogo from "./Springer (1).gif"
 
 export {IITRLogo, SSILogo, Pic1, Pic2, springerguide, conferenceregistration, springerlogo}
