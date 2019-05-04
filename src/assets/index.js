31c1bdd82b51bb1ef379e074497121c095eb0263
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2019-05-04 19:59:56 +0530

    Updated Carousel

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,8 +7,24 @@ import conferenceregistration from "./CONFERENCE REGISTRATION-converted.pdf"
 import springerlogo from "./Springer (1).gif"
 import posterprep from "./POSTER PREPARATION-converted.pdf"
 import DSC00242 from "./k.png"
-import top7 from "./top7.jpg"
 import top9 from "./top9.jpg"
+import carousel_01 from "./carousel_01.JPG"
+import carousel_02 from "./carousel_02.JPG"
+import carousel_03 from "./carousel_03.JPG"
+import carousel_04 from "./carousel_04.JPG"
+import carousel_05 from "./carousel_05.JPG"
+import carousel_06 from "./carousel_06.JPG"
+import carousel_07 from "./carousel_07.JPG"
+import carousel_08 from "./carousel_08.JPG"
+import carousel_09 from "./carousel_09.JPG"
+import carousel_10 from "./carousel_10.JPG"
+import carousel_11 from "./carousel_11.JPG"
+import carousel_12 from "./carousel_12.JPG"
+import carousel_13 from "./carousel_13.JPG"
+import carousel_14 from "./carousel_14.JPG"
+import carousel_15 from "./carousel_15.JPG"
+import carousel_16 from "./carousel_16.JPG"
+import carousel_17 from "./carousel_17.JPG"
 import ak from "./ak.png"
 import bk from "./bk.jpg"
 import vs from "./V-H-Saran.png"
@@ -16,4 +32,38 @@ import sp from "./surajfme_Fm2yrH3.jpg"
 import ap from "./drap1fme_Odt1rO5.jpg"
 import conferenceschedule from "./NSC 2019 schedule-converted (1).pdf"
 
-export {IITRLogo, SSILogo, Pic1, Pic2, springerguide, conferenceregistration, springerlogo, posterprep,DSC00242,top7,top9,ak,bk,vs,sp,ap,conferenceschedule}
+export {
+    IITRLogo,
+    SSILogo,
+    Pic1,
+    Pic2,
+    springerguide,
+    conferenceregistration,
+    springerlogo,
+    posterprep,
+    DSC00242,
+    top9,
+    carousel_01,
+    carousel_02,
+    carousel_03,
+    carousel_04,
+    carousel_05,
+    carousel_06,
+    carousel_07,
+    carousel_08,
+    carousel_09,
+    carousel_10,
+    carousel_11,
+    carousel_12,
+    carousel_13,
+    carousel_14,
+    carousel_15,
+    carousel_16,
+    carousel_17,
+    ak,
+    bk,
+    vs,
+    sp,
+    ap,
+    conferenceschedule
+}
